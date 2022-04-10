bf5ca401c8ce6a18235cf355ac9830be92e5ede1
michimichi2021 <michikaho1009@gmail.com>
2022-04-11 00:55:17 +0900

    stateのリフトアップ

--- src/index.js
+++ src/index.js
@@ -4,20 +4,13 @@ import './index.css';
 
 
 class Square extends React.Component {
-
-  constructor(props){
-    super(props);
-    this.state = {
-      value: null,
-    };
-  }
   render() {
     return (
-      <button 
-      className="square" 
-      onClick={() => this.setState({value: 'X'})}
+      <button
+      className="square"
+      onClick={() => this.props.onClick()}
       >
-        {this.state.value}
+        {this.props.value}
       </button>
     );
   }
@@ -26,8 +19,26 @@ class Square extends React.Component {
 
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
+
+  handleClick(i){
+    const squares = this.state.squares.slice();
+    squares[i]= 'X';
+    this.setState({squares: squares});
+  }
+
   renderSquare(i) {
-    return <Square value={i}/>;
+    return(
+    <Square
+      value={this.state.squares[i]}
+      onClick={()=>this.handleClick(i)}
+      />
+    );
   }
 
   render() {
